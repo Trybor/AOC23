608416b542685193477d10210bd9a3fb755f6587
Tibor Ivan Gebhartl <fhs51397@fh-salzburg.ac.at>
2023-12-09 19:20:09 +0100

    commiting task 2 as well ;)

--- Day 2/Program.cs
+++ Day 2/Program.cs
@@ -1,56 +1,95 @@
-﻿int runningTotal = 0;
-int maxRed = 12;
-int maxGreen = 13;
-int maxBlue = 14;
-
-string filePath = "input.txt";
+﻿string filePath = "input.txt";
 
 using (StreamReader reader = new StreamReader(filePath))
 {
+    int totalTask1 = 0;
+    int totalTask2 = 0;
+    int maxRed = 12;
+    int maxGreen = 13;
+    int maxBlue = 14;
     string line;
 
     while ((line = reader.ReadLine()) != null)
     {
-        var gameInfo = line.Split(':');
-        var gameId = int.Parse(gameInfo[0].Split(' ')[1]);
-        var rounds = gameInfo[1].Split(';', StringSplitOptions.TrimEntries);
-        bool isGameValid = true;
-        foreach (var round in rounds)
+        void task1()
         {
-            var colorInfos = round.Split(',', StringSplitOptions.TrimEntries);
-            foreach (var color in colorInfos)
+            var gameInfo = line.Split(':');
+            var gameId = int.Parse(gameInfo[0].Split(' ')[1]);
+            var rounds = gameInfo[1].Split(';', StringSplitOptions.TrimEntries);
+            bool isGameValid = true;
+            foreach (var round in rounds)
             {
-                var colorInfo = color.Split(' ');
-                int colorCount = int.Parse(colorInfo[0]);
-                string colorName = colorInfo[1];
-                if (colorName == "red")
+                var colorInfos = round.Split(',', StringSplitOptions.TrimEntries);
+                foreach (var color in colorInfos)
                 {
-                    if (colorCount > maxRed)
+                    var colorInfo = color.Split(' ');
+                    int colorCount = int.Parse(colorInfo[0]);
+                    string colorName = colorInfo[1];
+                    if (colorName == "red")
                     {
-                        isGameValid = false;
+                        if (colorCount > maxRed)
+                        {
+                            isGameValid = false;
+                        }
                     }
-                }
-                else if (colorName == "blue")
-                {
-                    if (colorCount > maxBlue)
+                    else if (colorName == "blue")
                     {
-                        isGameValid = false;
+                        if (colorCount > maxBlue)
+                        {
+                            isGameValid = false;
+                        }
                     }
-                }
-                else if (colorName == "green")
-                {
-                    if (colorCount > maxGreen)
+                    else if (colorName == "green")
                     {
-                        isGameValid = false;
+                        if (colorCount > maxGreen)
+                        {
+                            isGameValid = false;
+                        }
                     }
                 }
             }
+            if (isGameValid)
+            {
+                totalTask1 += gameId;
+            }
         }
-        if (isGameValid)
+        task1();
+        //----------------------------------------------------------//
+        void task2()
         {
-            runningTotal += gameId;
+            int maximumRed = 0;
+            int maximumGreen = 0;
+            int maximumBlue = 0;
+            var gameInfo = line.Split(':');
+            var gameId = int.Parse(gameInfo[0].Split(' ')[1]);
+            var rounds = gameInfo[1].Split(';', StringSplitOptions.TrimEntries);
+            foreach (var round in rounds)
+            {
+                var colorInfos = round.Split(',', StringSplitOptions.TrimEntries);
+                foreach (var color in colorInfos)
+                {
+                    var colorInfo = color.Split(' ');
+                    int colorCount = int.Parse(colorInfo[0]);
+                    string colorName = colorInfo[1];
+                    if (colorName == "red")
+                    {
+                        maximumRed = Math.Max(colorCount, maximumRed);
+                    }
+                    else if (colorName == "blue")
+                    {
+                        maximumBlue = Math.Max(colorCount, maximumBlue);
+                    }
+                    else if (colorName == "green")
+                    {
+                        maximumGreen = Math.Max(colorCount, maximumGreen);
+                    }
+                }
+            }
+            var lineResult = maximumRed * maximumBlue * maximumGreen;
+            totalTask2 += lineResult;
         }
+        task2();
     }
+    System.Console.WriteLine(totalTask1);
+    System.Console.WriteLine(totalTask2);
 }
-
-System.Console.WriteLine(runningTotal);

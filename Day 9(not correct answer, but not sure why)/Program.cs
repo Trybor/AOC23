5fc72f604e409cd0ad8cb7d381f27a7cb14501d0
Tibor Ivan Gebhartl <fhs51397@fh-salzburg.ac.at>
2023-12-09 11:24:15 +0100

    commting day 9 but its not working

---
+++ Day 9(not correct answer, but not sure why)/Program.cs
@@ -0,0 +1,18 @@
+﻿int totalSum = 0;
+
+string file = "input.txt";
+string[] lines = File.ReadAllLines(file);
+
+foreach (string line in lines)
+{
+    int[] rowValues = Array.ConvertAll(line.Split(' '), int.Parse);
+    int[,] matrix = new int[line.Length, rowValues.Length];
+
+    for (int i = 0; i < rowValues.Length; i++)
+    {
+        matrix[0, i] = rowValues[i];
+    }
+
+    totalSum += matrix[0, matrix.GetLength(1) - 1];
+}
+Console.WriteLine("Total sum of all sums: " + totalSum);
